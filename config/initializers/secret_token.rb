5ac297e25e1ae0f4c54f434e5b861724f8b0c5eb
shanghaichris <shanghaichris@gmail.com>
2011-02-25 19:56:16 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TripPlanner::Application.config.secret_token = '5fafb15456a6b5b4df9061fae953a58c38f0f36c441f93b5932c98d1b0658f65e90408b04b8fe0cdec9ef7bcf39f95ed629901197974630cb2c2ad2990c3e9e8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TripPlanner::Application.config.session_store :cookie_store, :key => '_TripPlanner_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TripPlanner::Application.config.session_store :active_record_store
